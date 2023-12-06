7a95b67f2c8974a9c04dfec5f2109dcafcde11cc
Praladh Chaulagain <p.chaulagain001@umb.edu>
2023-12-06 18:38:58 -0500

    Updated README.md (or a descriptive message)

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,7 +3,7 @@ import {useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import AnimatedLetters from '../AnimatedLetters'
 import Logo from './Logo'
-import LogoTitle from '../../assets/images/logo-s.png'
+import LogoTitle from '../../assets/images/logo-p1.png'
 import './index.scss'
 import Loader from 'react-loaders'
 
